45a00ec716ce4c1ed02e1cfa36c0470b6f588e88
스터디하자 <hmin5015@gmail.com>
2023-09-10 06:15:40 -0700

    Add Form & Button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
+import Form from './Form';
+
 function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  const [reqType, setReqType] = useState('users');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL + reqType}`);
+        const data = await response.json();
+        console.log(data);
+        setItems(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchItems();
+  }, [reqType]);
 
   return (
     <div className="App">
+      <Form reqType={reqType} setReqType = {setReqType} />
+      {/* <List items={items} /> */}
     </div>
   );
 }
